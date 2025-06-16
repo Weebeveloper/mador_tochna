3a53c9152ca5011f28ce728965bd7d131fdb653e
Weebeveloper <aaas.com.com@gmail.com>
2025-06-16 11:14:10 +0300

    free search build

--- src/app/pages/guides/guides.page.ts
+++ src/app/pages/guides/guides.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { combineLatest, filter, map, startWith, tap } from 'rxjs';
 import { MetaDataService } from 'src/app/domain/services/metadata.service';
 
 @Component({
@@ -7,5 +9,21 @@ import { MetaDataService } from 'src/app/domain/services/metadata.service';
   styleUrls: ['./guides.page.scss'],
 })
 export class GuidesPageComponent {
-  constructor(readonly metadata: MetaDataService) {}
+  filters = new FormGroup({
+    search: new FormControl(''),
+  });
+
+  readonly dataCards$ = combineLatest([
+    this._metadata.dataCards$,
+    this.filters.controls.search.valueChanges.pipe(startWith('')),
+  ]).pipe(
+    map(([cards, search]) =>
+      cards.filter(
+        (card) =>
+          card.name.includes(search!) || card.description.includes(search!)
+      )
+    )
+  );
+
+  constructor(private readonly _metadata: MetaDataService) {}
 }
--- src/app/pages/guides/guidesPage.module.ts
+++ src/app/pages/guides/guidesPage.module.ts
@@ -3,9 +3,19 @@ import { GuidesPageComponent } from './guides.page';
 import { TestingPageRouting } from './guidesPage-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
   declarations: [GuidesPageComponent],
-  imports: [CommonModule, TestingPageRouting, SharedModule],
+  imports: [
+    CommonModule,
+    TestingPageRouting,
+    SharedModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
 })
 export class GuidesPageModule {}
